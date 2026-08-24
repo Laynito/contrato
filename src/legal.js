54740d7e965a043a6837069d676196b280bc4cbb
Antonio Laynes <32025143+Laynito@users.noreply.github.com>
2026-08-24 07:08:46 -0700

    feat: add versioned legal constants

---
+++ src/legal.js
@@ -0,0 +1,44 @@
+export const RULES_VERSION = 'MX-LFT-2026.1';
+export const TEMPLATE_VERSION = 'CONTRATO-V1.0';
+
+export const WEEKLY_HOURS_BY_YEAR = Object.freeze({
+  2026: 48,
+  2027: 46,
+  2028: 44,
+  2029: 42,
+  2030: 40,
+});
+
+export const WEEKLY_OVERTIME_BY_YEAR = Object.freeze({
+  2026: 9,
+  2027: 10,
+  2028: 11,
+  2029: 12,
+  2030: 12,
+});
+
+export const MINIMUM_WAGE_2026 = Object.freeze({
+  general: 315.04,
+  zlfn: 440.87,
+});
+
+export const MINIMUM_BENEFITS = Object.freeze({
+  aguinaldoDays: 15,
+  vacationPremiumPercent: 25,
+  firstYearVacationDays: 12,
+});
+
+export const RELATION_TYPES = Object.freeze([
+  'INDETERMINADO',
+  'DETERMINADO',
+  'OBRA',
+  'TEMPORADA',
+  'PRUEBA',
+  'CAPACITACION_INICIAL',
+]);
+
+export const SPECIAL_REVIEW_FLAGS = Object.freeze([
+  'MENOR_EDAD',
+  'NO_COMPETENCIA_POST_EMPLEO',
+  'TRABAJO_ESPECIAL',
+]);
